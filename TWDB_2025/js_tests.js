96560c51da00d669258df048b82ed05d5130d456
Alysson <alysson.dg@hotmail.com>
2025-12-15 20:48:01 +0100

    guessing game

--- TWDB_2025/js_tests.js
+++ TWDB_2025/js_tests.js
@@ -1,7 +1,27 @@
-console.log("Hello, world!");
-
-for (let i = 0; i < 6; i++) {
-  console.log('Da ba dee da ba daa');
+let maximum = parseInt(prompt("Enter the maximum number:"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter a valid number:"));
 }
 
+const targetNum = Math.floor(Math.random() * maximum) + 1;
 
+let guess = parseInt(prompt("Enter your first guess:"));
+let attempts = 1; 
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  attempts++; 
+
+  if (parseInt(guess) > targetNum) {
+    guess = prompt("Too high! Enter a new guess:");
+  } else {
+    guess = prompt("Too low! Enter a new guess:");
+  } 
+}
+
+  if (guess === "q") {
+    console.log("You quit the game.");
+  } else {
+    console.log(`Congrats! You guessed the number correctly!`);
+    console.log(`You guessed ${attempts} times. Try again!`);
+  }
